0abd51705f04a8a7ae62dfb24331f7b4bb82d31b
GERALD <kiemdegeraldo9@gmail.com>
2024-11-30 22:32:16 +0000

    vs2

--- APPSECU/APPSECU/securité/urls.py
+++ APPSECU/APPSECU/securité/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('voiture/', views.voiture, name='voiture'),
     path('ajout_voiture/', views.ajout_voiture, name='create_voiture'),
+    path('modifier_voiture/<int:pk>/', views.modifier_voiture, name='modifier_voiture'),
+    path('supprimer_voiture/<int:pk>/', views.supprimer_voiture, name='supprimer_voiture'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- APPSECU/APPSECU/securité/views.py
+++ APPSECU/APPSECU/securité/views.py
@@ -1,6 +1,6 @@
 
 # Create your views here.
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from .forms import VoitureForm
 from .models import Voiture
 
@@ -18,11 +18,42 @@ def voiture(request):
 
 
 def ajout_voiture(request):
-    form = VoitureForm()
-    if request.method == 'POST':
+    if request.method == "POST":
         form = VoitureForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'La voiture a été ajoutée avec succès!')
-            return redirect('ajout_voiture')
+            form.save()  # Enregistrer les données dans la base
+            messages.success(request, "Voiture ajoutée avec succès !")
+            return redirect('voiture')  # Redirige vers la liste des voitures
+        else:
+            messages.error(request, "Erreur lors de l'ajout de la voiture. Vérifiez les données.")
+    else:
+        form = VoitureForm()
+
     return render(request, 'fichier/creer_voiture.html', {'form': form})
+
+def modifier_voiture(request, pk):
+    voiture = Voiture.objects.get(pk=pk)
+
+    if request.method == "POST":
+        form = VoitureForm(request.POST, instance=voiture)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Voiture modifiée avec succès !")
+            return redirect('voiture')  # Redirige vers la liste des voitures
+        else:
+            messages.error(request, "Erreur lors de la modification de la voiture. Vérifiez les données.")
+    else:
+        form = VoitureForm(instance=voiture)
+
+    return render(request, 'fichier/modifier_voiture.html', {'form': form, 'voiture': voiture})
+
+
+def supprimer_voiture(request, pk):
+    voiture = get_object_or_404(Voiture, pk=pk)
+    
+    if request.method == "POST":
+        voiture.delete()
+        messages.success(request, "Voiture supprimée avec succès !")
+        return redirect('voiture')
+    
+    return render(request, 'fichier/supprimer_voiture.html', {'voiture': voiture})
